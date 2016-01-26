4ad4f5e55e91afb1ca556cfdfbc80398b4f201dd
Allan <amaxwellblair@gmail.com>
2016-01-26 11:47:47 -0700

    figured out basic private and public encrypt decrypt using PKSC1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Clarke Coin Project
+Basic implementation of a cryptocurrency dubbed Clarke Coin using Golang
+
+Original idea/outline here: https://github.com/worace/coinage.git
+
+#### Cryptography
+- Looking to explore and understand cryptography a bit better
+- Specifically:
+  - Hashing
+  - RSA public and private keys
+  - Signing / Verifying
+
+#### Data Structures and Algorithms
+- TBD
--- cryptography_fun.go
+++ cryptography_fun.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+    "fmt"
     "crypto/rsa"
     "crypto/rand"
-    "crypto/sha256"
+    // "reflect"
+    // "crypto/sha256"
 )
 
 func main() {
+    // Had some issues with relfection and pointers - figured it out though!
+    // publicKey := new(rsa.PublicKey)
+    // fakeKey := privateKey.Public()
+    // fakeType := reflect.TypeOf(fakeKey)
+    // fakeValue := reflect.ValueOf(fakeKey)
+    // fmt.Println("Reflection of fake key type: ", fakeType)
+    // fmt.Println("Reflection of fake key value:", fakeValue)
     privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
-    publicKey := privateKey.Public
+    publicKey := privateKey.Public().(*rsa.PublicKey)
+
+    basicPubEncrypt, _ := rsa.EncryptPKCS1v15(rand.Reader, publicKey, []byte("Hello world!"))
+    fmt.Println("RSA encryption using a public key: ", string(basicPubEncrypt))
+
+    basicPrivateDecrypt, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, basicPubEncrypt)
+    fmt.Println("RSA decryption using a private key: ", string(basicPrivateDecrypt))
+
+
+
 }
