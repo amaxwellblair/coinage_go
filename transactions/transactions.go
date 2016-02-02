25ab7e7294d83854360883b90295642518f2a8a7
Allan <amaxwellblair@gmail.com>
2016-02-01 20:35:21 -0700

    added input output structs and created the general transaction structure

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -1,5 +1,49 @@
 package transactions
 
 import(
-    "github.com/amaxwellblair/coinage_go/keyGenerator"
+    // "github.com/amaxwellblair/coinage_go/keyGenerator"
+    "time"
 )
+
+type Transaction struct {
+    Inputs []Input
+    Outputs []Output
+    TimeStamp int
+    Hash string
+}
+
+type Input struct {
+    SourceHash string
+    SourceIndex int
+    Signature string
+}
+
+type Output struct {
+    Amount int
+    Address string
+}
+
+func CreateTime() int {
+    currentTime := time.Now()
+    epochTime := time.Parse(time.RFC822, "01 Jan 70 00:00 MST")
+}
+
+func (t * Transaction) StampTransaction() {
+
+}
+
+func New() (*Transaction) {
+    return new(Transaction)
+}
+
+func (t *Transaction) CreateOutput(amount int, address string) (Output) {
+    t.Outputs = append(t.Outputs, Output{amount, address})
+    return t.Outputs[len(t.Outputs)-1]
+}
+
+func (t *Transaction) CreateInput(sourcehash string, sourceindex int, signature string) (Input) {
+    t.Inputs = append(t.Inputs, Input{sourcehash, sourceindex, signature})
+    return t.Inputs[len(t.Inputs)-1]
+}
+
+// {Amount: amount, Address: address}
--- transactions/transactions_test.go
+++ transactions/transactions_test.go
@@ -6,7 +6,34 @@ import(
 )
 
 
-func TestTransaction_CreateInputs_BasicExistenceCheck(t *testing.T)  {
-    t := New()
-    t.CreateInput(5, )
+func TestTransaction_CreateOutputs_BasicExistenceCheck(t *testing.T) {
+    trans := New()
+    ck := keygen.NewClarkeKey()
+    output := trans.CreateOutput(5, ck.PublicPem)
+    if output.Address != ck.PublicPem {
+        t.Fatal("The output address is not returned")
+    } else if output.Amount != 5 {
+        t.Fatal("The output amount is not returned")
+    }
+}
+
+func TestTransaction_CreateInputs_BasicExistenceCheck(t *testing.T) {
+    trans := New()
+    input := trans.CreateInput("sourcehash", 0, "signature")
+    if input.SourceHash != "sourcehash" {
+        t.Fatal("Source Hash doesn't work")
+    }
+    if input.SourceIndex != 0 {
+        t.Fatal("Source Index doesn't work")
+    }
+    if input.Signature != "signature" {
+        t.Fatal("Signature doesn't work")
+    }
+}
+
+func TestCreateTime_MakeSureTimeCreationWorks(t *testing.T) {
+    time := CreateTime()
+    if time <= 0 {
+        t.Fatal("Create time didn't work properly")
+    }
 }
