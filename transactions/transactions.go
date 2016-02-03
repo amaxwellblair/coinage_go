0762df89311328d348b890b903b5faf13a1b8d86
Allan <amaxwellblair@gmail.com>
2016-02-03 12:39:40 -0700

    added input & output to bytes

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -3,12 +3,16 @@ package transactions
 import(
     // "github.com/amaxwellblair/coinage_go/keyGenerator"
     "time"
+    // "fmt"
+    "reflect"
+    "strconv"
+    "errors"
 )
 
 type Transaction struct {
     Inputs []Input
     Outputs []Output
-    TimeStamp int
+    TimeStamp float64
     Hash string
 }
 
@@ -23,13 +27,58 @@ type Output struct {
     Address string
 }
 
-func CreateTime() int {
-    currentTime := time.Now()
-    epochTime := time.Parse(time.RFC822, "01 Jan 70 00:00 MST")
+func CreateTime() float64 {
+    epochTime, err := time.Parse(time.RFC822, "01 Jan 70 00:00 MST")
+    if err != nil {
+        panic(err)
+    }
+    passedTime := time.Since(epochTime)
+    totalSeconds := passedTime.Seconds()
+    return totalSeconds * 1000
 }
 
-func (t * Transaction) StampTransaction() {
+func (t *Transaction) StampTransaction() {
 
+}
+
+func (t *Transaction) fieldsInBytes() {
+
+}
+
+func (i *Input) inputInBytes() ([]byte) {
+    var inputBytes []byte
+    value := reflect.ValueOf(*i)
+    for i := 0; i < value.NumField(); i++ {
+        intermediary := value.Field(i).Interface()
+        switch intermediary.(type) {
+        case string:
+            inputBytes = append(inputBytes, []byte(intermediary.(string))...)
+        case int:
+            intermediateString := strconv.Itoa(intermediary.(int))
+            inputBytes = append(inputBytes, []byte(intermediateString)...)
+        default:
+            panic(errors.New("Encountered an input type that wasn't recognized"))
+        }
+    }
+    return inputBytes
+}
+
+func (o *Output) outputInBytes() ([]byte) {
+    var outputBytes []byte
+    value := reflect.ValueOf(*o)
+    for o := 0; o < value.NumField(); o++ {
+        intermediary := value.Field(o).Interface()
+        switch intermediary.(type) {
+        case string:
+            outputBytes = append(outputBytes, []byte(intermediary.(string))...)
+        case int:
+            intermediateString := strconv.Itoa(intermediary.(int))
+            outputBytes = append(outputBytes, []byte(intermediateString)...)
+        default:
+            panic(errors.New("Encountered an output type that wasn't recognized"))
+        }
+    }
+    return outputBytes
 }
 
 func New() (*Transaction) {
--- transactions/transactions_test.go
+++ transactions/transactions_test.go
@@ -3,13 +3,17 @@ package transactions
 import(
     "testing"
     "github.com/amaxwellblair/coinage_go/keyGenerator"
+    // "crypto/sha256"
+    // "bytes"
 )
 
 
 func TestTransaction_CreateOutputs_BasicExistenceCheck(t *testing.T) {
     trans := New()
     ck := keygen.NewClarkeKey()
+
     output := trans.CreateOutput(5, ck.PublicPem)
+
     if output.Address != ck.PublicPem {
         t.Fatal("The output address is not returned")
     } else if output.Amount != 5 {
@@ -19,7 +23,9 @@ func TestTransaction_CreateOutputs_BasicExistenceCheck(t *testing.T) {
 
 func TestTransaction_CreateInputs_BasicExistenceCheck(t *testing.T) {
     trans := New()
+
     input := trans.CreateInput("sourcehash", 0, "signature")
+
     if input.SourceHash != "sourcehash" {
         t.Fatal("Source Hash doesn't work")
     }
@@ -33,7 +39,53 @@ func TestTransaction_CreateInputs_BasicExistenceCheck(t *testing.T) {
 
 func TestCreateTime_MakeSureTimeCreationWorks(t *testing.T) {
     time := CreateTime()
-    if time <= 0 {
+
+    if time <= 1400000000000 {
         t.Fatal("Create time didn't work properly")
     }
+}
+
+func TestInput_inputInBytes_AllOftheInputsTogetherInAByteSlice(t *testing.T) {
+    trans := New()
+    input := trans.CreateInput("sourcehash", 0, "signature")
+
+    inputBytes := input.inputInBytes()
+
+    if string(inputBytes) != "sourcehash0signature" {
+        t.Fatal("Input to bytes doesn't work too well: ", inputBytes)
+    }
+}
+
+func TestOutput_outputInBytes_AllOftheOutputsTogetherInAByteSlice(t *testing.T) {
+    trans := New()
+    output := trans.CreateOutput(5, "address")
+
+    outputBytes := output.outputInBytes()
+
+    if string(outputBytes) != "5address" {
+        t.Fatal("Output to bytes doesn't work too well: ", outputBytes)
+    }
+}
+
+func TestTransaction_StampTransaction_MakeSureTheTransactionIsHashedAndTimeStamped(t *testing.T) {
+    // trans := New()
+    // trans.CreateInput("sourcehash", 0, "signature")
+    // trans.CreateOutput(5, "Publickey")
+    // trans.StampTransaction()
+    // if trans.TimeStamp <= 1400000000000 {
+    //     t.Fatal("TimeStamp wasn't properly set")
+    // }
+    //
+    // inputs := trans.Inputs[0].SourceHash + trans.Inputs[0].SourceIndex trans.Input[0].Signature
+    // outputs := trans.Outputs[0].Amount + trans.Outputs[0].Address + trans.TimeStamp
+    //
+    // hashString := inputs + outputs
+    // shaNew := sha256.New()
+    //
+    // var buffer []byte
+    // shaNew.Write(buffer)
+    //
+    // if trans.Hash == "hash"{
+    //     t.Fatal("The hash isn't working properly")
+    // }
 }
